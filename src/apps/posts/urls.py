fa705cbae06fdf78899f18bdc404a8a675fabe62
krodev <kfd.dev@gmail.com>
2017-06-12 21:35:09 -0400

    add delete, redirect, y mensaje

--- src/apps/posts/urls.py
+++ src/apps/posts/urls.py
@@ -3,10 +3,10 @@ from . import views
 #solo . porque es una importacion relativa, esta en la misma carpeta
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'^$', views.post_list),
+    url(r'^$', views.post_list, name='list'),
     url(r'^create/$', views.post_create),
     url(r'^(?P<id>\d+)/$', views.post_detail,name='detail'),
     url(r'^(?P<id>\d+)/edit/$', views.post_update, name='update'),
-    url(r'^delete/$', views.post_delete),
+    url(r'^(?P<id>\d+)/delete/$', views.post_delete),
 #ordenar las url
 ]
